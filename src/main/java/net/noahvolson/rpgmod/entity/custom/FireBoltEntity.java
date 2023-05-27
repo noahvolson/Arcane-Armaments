f8d7a8a35423de1fbf09e1a6e2409f65066149dd
noahvolson <noah.v.olson@gmail.com>
2023-05-26 21:31:10 -0400

    Fixed firebolt sounds, removed stuck arrows

--- src/main/java/net/noahvolson/rpgmod/entity/custom/FireBoltEntity.java
+++ src/main/java/net/noahvolson/rpgmod/entity/custom/FireBoltEntity.java
@@ -1,19 +1,18 @@
 package net.noahvolson.rpgmod.entity.custom;
 
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
-import net.minecraftforge.common.ForgeConfig;
 import net.minecraftforge.network.NetworkHooks;
 import net.noahvolson.rpgmod.particle.ModParticles;
 import org.jetbrains.annotations.NotNull;
@@ -21,38 +20,46 @@ import org.jetbrains.annotations.NotNull;
 public class FireBoltEntity extends AbstractArrow {
     public FireBoltEntity(EntityType<FireBoltEntity> entityType, Level world) {
         super(entityType, world);
+        this.playSound(SoundEvents.GHAST_SHOOT, 1.0F, 1.2F / (this.random.nextFloat() * 0.2F + 0.9F));
     }
 
     public FireBoltEntity(EntityType<FireBoltEntity> entityType, double x, double y, double z, Level world) {
         super(entityType, x, y, z, world);
+        this.playSound(SoundEvents.GHAST_SHOOT, 1.0F, 1.2F / (this.random.nextFloat() * 0.2F + 0.9F));
     }
 
     public FireBoltEntity(EntityType<FireBoltEntity> entityType, LivingEntity shooter, Level world) {
         super(entityType, shooter, world);
+        this.playSound(SoundEvents.GHAST_SHOOT, 1.0F, 1.2F / (this.random.nextFloat() * 0.2F + 0.9F));
     }
 
     @Override
     protected void onHitEntity(@NotNull EntityHitResult ray) {
+        this.setSoundEvent(new SoundEvent(new ResourceLocation("none")));
         super.onHitEntity(ray);
         // this, x, y, z, explosionStrength, setsFires, breakMode
         this.level.explode(this, this.getX(), this.getY(), this.getZ(), 1f, true, Explosion.BlockInteraction.NONE);
+
+        Entity target = ray.getEntity();
+
+        // Remove arrows that may have been added by the hit
+        if (target instanceof LivingEntity) {
+            if (!this.level.isClientSide && this.getPierceLevel() <= 0) {
+                LivingEntity livingentity = (LivingEntity)target;
+                livingentity.setArrowCount(livingentity.getArrowCount() - 1);
+            }
+        }
+
         ray.getEntity().setSecondsOnFire(4);
+
     }
 
     @Override
     protected void onHitBlock(@NotNull BlockHitResult ray) {
+        this.setSoundEvent(new SoundEvent(new ResourceLocation("none")));
         super.onHitBlock(ray);
         // To explode on hit:
         this.level.explode(this, this.getX(), this.getY(), this.getZ(), 1f, true, Explosion.BlockInteraction.NONE);
-    }
-
-    // To blow up after 3 seconds
-    @Override
-    protected void tickDespawn() {
-        if (this.inGroundTime > 60){
-            //this.level.explode(this, this.getX(), this.getY(), this.getZ(), 4.0f, true, Explosion.BlockInteraction.NONE);
-            this.discard();
-        }
     }
 
     // Returns the item stack to give the player
@@ -79,8 +86,6 @@ public class FireBoltEntity extends AbstractArrow {
 
     private void makeParticle() {
         for(int j = 0; j < 5; ++j) {
-            //this.level.addParticle(ParticleTypes.FLAME, this.getX(), this.getY(), this.getZ(), 0, 0, 0);
-            //this.getRandomX(0.5D), this.getRandomY(), this.getRandomZ(0.5D)\
 
             double magnitude = .03;
             double xD = (2*Math.random() - 1) * magnitude;
