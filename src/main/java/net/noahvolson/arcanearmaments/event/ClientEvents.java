97d30e5d9852ddf33bdaadad1ac459ea32cdadd9
noahvolson <noah.v.olson@gmail.com>
2024-03-20 20:16:05 -0400

    Hide trinket bar until implemented

--- src/main/java/net/noahvolson/arcanearmaments/event/ClientEvents.java
+++ src/main/java/net/noahvolson/arcanearmaments/event/ClientEvents.java
@@ -65,7 +65,7 @@ public class ClientEvents {
             event.registerBelowAll("hud_moved_air", ModHudOverlay.MOVED_AIR);
             event.registerBelowAll("hud_moved_armor", ModHudOverlay.MOVED_ARMOR);
             event.registerBelowAll("hud_berserk", ModHudOverlay.HUD_BERSERK);
-            event.registerBelowAll("hud_trinket_hotbar", ModHudOverlay.HUD_TRINKET_HOTBAR);
+            //event.registerBelowAll("hud_trinket_hotbar", ModHudOverlay.HUD_TRINKET_HOTBAR);
             event.registerBelowAll("hud_class_hotbar", ModHudOverlay.HUD_CLASS_HOTBAR);
             event.registerAboveAll("hud_venom", ModHudOverlay.HUD_VENOM);
             event.registerAboveAll("hud_cooldowns", ModHudOverlay.HUD_COOLDOWNS);
