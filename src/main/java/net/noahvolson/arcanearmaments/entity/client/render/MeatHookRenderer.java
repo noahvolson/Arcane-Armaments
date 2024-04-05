745fe3b32fe992ae44fe4ae5ed50a0bfdf4708b6
noahvolson <noah.v.olson@gmail.com>
2024-04-04 23:36:43 -0400

    Fix grapple hook rotation

--- src/main/java/net/noahvolson/arcanearmaments/entity/client/render/MeatHookRenderer.java
+++ src/main/java/net/noahvolson/arcanearmaments/entity/client/render/MeatHookRenderer.java
@@ -1,5 +1,8 @@
 package net.noahvolson.arcanearmaments.entity.client.render;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -8,6 +11,7 @@ import net.noahvolson.arcanearmaments.ArcaneArmaments;
 import net.noahvolson.arcanearmaments.entity.client.model.MeatHookModel;
 import net.noahvolson.arcanearmaments.entity.skill.warrior.GrappleAttack;
 import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class MeatHookRenderer extends GeoEntityRenderer<GrappleAttack> {
@@ -26,5 +30,13 @@ public class MeatHookRenderer extends GeoEntityRenderer<GrappleAttack> {
     @Override
     public RenderType getRenderType(GrappleAttack animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
         return super.getRenderType(animatable, texture, bufferSource, partialTick);
+    }
+
+    @Override
+    // Need to override the default LivingEntity rendering behavior (as this is a projectile)
+    public void actuallyRender(PoseStack poseStack, GrappleAttack animatable, BakedGeoModel model, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        poseStack.mulPose(Axis.YP.rotationDegrees(animatable.getYRot()));
+        poseStack.mulPose(Axis.XP.rotationDegrees(-animatable.getXRot()));
+        super.actuallyRender(poseStack, animatable, model, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
